a8f191d66edb52e0b133c2916e7df15188a6afc8
dongwoo <siwol406@gmail.com>
2024-08-26 04:56:31 +0900

    ddd

--- StudyMyself/ddd/src/app.module.ts
+++ StudyMyself/ddd/src/app.module.ts
@@ -3,17 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MemberModule } from './member/member.module';
 import { OrderModule } from './order/order.module';
-import { PostModule } from './post/post.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Order } from './order/domain/entities/order.entity';
-import { Post } from './post/entities/post.entity';
 import { MemberEntity } from './member/domain/entities/member.entity';
 
 @Module({
   imports: [
     MemberModule,
     OrderModule,
-    PostModule,
     TypeOrmModule.forRoot({
       type: 'postgres',
       host: 'localhost',
@@ -21,7 +18,7 @@ import { MemberEntity } from './member/domain/entities/member.entity';
       username: 'postgres',
       password: '1234',
       database: 'postgres',
-      entities: [MemberEntity, Order, Post],
+      entities: [MemberEntity, Order],
       synchronize: true,
       dropSchema: true,
     }),
